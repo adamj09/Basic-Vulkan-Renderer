8d5c17615fbfc9541572d58794090dcad701f2b1
AdamJ <adamjohnston144@gmail.com>
2023-03-11 12:22:16 -0500

    changed api version to 1.2

--- source/engine/device/device.cpp
+++ source/engine/device/device.cpp
@@ -31,7 +31,7 @@ namespace Renderer{
     void Device::createInstance(){
         VkApplicationInfo appInfo = {};
         appInfo.sType =VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.apiVersion = VK_API_VERSION_1_3;
+        appInfo.apiVersion = VK_API_VERSION_1_2;
         appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
         appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
         appInfo.pApplicationName = "Renderer";
